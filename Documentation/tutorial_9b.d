1f9d8f31a207f079bd286681c4b8c30479b401a6
Mike Riley <mikehriley@hotmail.com>
2020-06-02 06:43:12 -0700

    Added tutorial_9b.  This tutorial shows how to setup the simulator for doing tutorial 9 starting in Lunar Orbit.

---
+++ Documentation/tutorial_9b.d
@@ -0,0 +1,50 @@
+[c]	Starting at the Moon
+[=]
+	This short tutorial will show you how to setup the simulator for a
+	Lunar landing starting in Lunar orbit.  This is to prepare you for
+	tutorial 9, which shows how to land on the Moon from Lunar orbit.
+[=]
+	To begin make sure you do not currently have a lms.sav file.  If this
+	file exists then the simulator will load it rather than allowing you
+	to create a new simulation.
+[=]
+	1. Start the simulator and you will be presented with a short menu
+	of options for starting the simulation.  Select option '2' to start
+	from the Moon.
+[=]
+	2. Next you will be given a list of spacecraft to fly.  Select option
+	'2', the Apollo-J.  The landing tutorial assumes this spacecraft is the
+	one being flown.
+[=]
+	3. Next you will be asked for a 'Target Longitude', this is the longitude
+	of your desired landing site.  We will setup to use the Apollo 11 landing
+	site as our target, so enter 23.47.
+[=]
+	4. Next you will be asked for the 'Target Latitude'.  The latitude of
+	the Apollo 11 landing site is 0.67n, so enter 0.67.
+[=]
+	5. Next you will be asked for 'CSM Longitude Ascending Node'.  This is
+	the point in the orbit where the CSM crosses from the southern hemisphere
+	to the northern.  For the simplest in-plane landing at the Apollo 11
+	landing site we want to set the LAN 90 degrees East of the landing site.
+	Since the landing site is 23.47 degrees East, the LAN needs to be at
+	113.47 degrees East, so enter 113.47.
+[=]
+	6. And last you will be asked for 'CSM Inclination'.  This is the inclination
+	of our orbit relative to the Lunar Equator.  For the simplest in-plane landing
+	we want to set this to the same as our target latitude, so set 0.67 here.
+[=]
+	Now you will be taken into the simulator in orbit of the Moon.  When starting
+	from the Moon the simulator uses a greatly simplified CSM.  This CSM cannot
+	be maneuvered, it will be oriented already as described in tutorial 9, so you
+	will not need to perform this step yourself.
+[=]
+	The simplified CSM also uses a different version of the guidance computer and
+	so the commands used to set computer modes in tutorial 9 cannot be used.
+	When asked to set V16N12 in the tutorial, just press the <2> key and the
+	simplified guidance computer will be in a mode where you get your position
+	relative to the landing site as needed in the tutorial.
+[=]
+	At this point go back to tutorial 9 and good luck on your landing!
+[=]
+
